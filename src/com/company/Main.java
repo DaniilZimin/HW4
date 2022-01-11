aa05ab7c4d0b115356cd0f6ab98ace41d9fe0d2e
Iam <user@gmail.com>
2022-01-11 22:34:21 +0500

    Выполнил дз№4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,69 +11,66 @@ public class Main {
         double[] numbers2 = {1.57, 7.654, 9.986};
         String[] names = {"Иван", "Николай", "Софья", "Елесей"};
 
-        for (int i = 0; i < numbers1.length; i++){
+        for (int i = 0; i < numbers1.length; i++) {
             System.out.print(numbers1[i]);
-            if(i < (numbers1.length - 1)){
+            if (i < (numbers1.length - 1)) {
                 System.out.print(", ");
-            }else {
+            } else {
                 System.out.println();
             }
         }
-        for (int i = 0; i < numbers2.length; i++){
+        for (int i = 0; i < numbers2.length; i++) {
             System.out.print(numbers2[i]);
-            if(i < (numbers2.length - 1)){
+            if (i < (numbers2.length - 1)) {
                 System.out.print(", ");
-            }else {
+            } else {
                 System.out.println();
             }
         }
-        for (int i = 0; i < names.length; i++){
+        for (int i = 0; i < names.length; i++) {
             System.out.print(names[i]);
-            if(i < (names.length - 1)){
+            if (i < (names.length - 1)) {
                 System.out.print(", ");
-            }else {
+            } else {
                 System.out.println();
             }
         }
-        for (int i = numbers1.length - 1; i >= 0; i--){
+        for (int i = numbers1.length - 1; i >= 0; i--) {
             System.out.print(numbers1[i]);
-            if(i > 0){
+            if (i > 0) {
                 System.out.print(", ");
-            }else {
+            } else {
                 System.out.println();
             }
         }
-        for (int i = numbers2.length - 1; i >= 0; i--){
+        for (int i = numbers2.length - 1; i >= 0; i--) {
             System.out.print(numbers2[i]);
-            if(i > 0){
+            if (i > 0) {
                 System.out.print(", ");
-            }else {
+            } else {
                 System.out.println();
             }
         }
-        for (int i = names.length - 1; i >= 0; i--){
+        for (int i = names.length - 1; i >= 0; i--) {
             System.out.print(names[i]);
-            if(i > 0){
+            if (i > 0) {
                 System.out.print(", ");
-            }else {
+            } else {
                 System.out.println();
             }
         }
 
-        for (int i = 0; i < numbers1.length; i++){
-            if(numbers1[i]%2 != 0){
-                numbers1[i]+=1;
+        for (int i = 0; i < numbers1.length; i++) {
+            if (numbers1[i] % 2 != 0) {
+                numbers1[i] += 1;
             }
             System.out.print(numbers1[i]);
-            if(i < (numbers2.length - 1)){
+            if (i < (numbers2.length - 1)) {
                 System.out.print(", ");
-            }else {
+            } else {
                 System.out.println();
             }
         }
-
-
-
 
 
     }
